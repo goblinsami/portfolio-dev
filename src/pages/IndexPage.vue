dfbd34baf8e5dd0adc2323db0a23fc7d458d32f5
sami janafse <goblinsami@gmail.com>
2023-05-17 17:22:49 +0200

    imgss

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page class="flex flex-center">
-  </q-page>
+    <!--     <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+ --> </q-page>
 </template>
 
 <script>
